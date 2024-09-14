41fde847e190385b54adb92b2207f15d10931b30
Gourav Nayar <127810086+GOURAVNAYAR@users.noreply.github.com>
2024-09-14 16:49:04 +0530

    Update JDBCConnection.java

--- src/com/connection/JDBCConnection.java
+++ src/com/connection/JDBCConnection.java
@@ -4,13 +4,13 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
-public class JDBCConnection {
+public class JDBCConnection {  //connection
 
 	public static Connection getConnection() throws ClassNotFoundException, SQLException {
 		//Load Driver
 		Class.forName("com.mysql.jdbc.Driver");
 		//Create Connection
 		Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/ecorse_batch_2?characterEncoding=utf8", "root", "root");
-		return con;
+		return con;   
 	}
 }
